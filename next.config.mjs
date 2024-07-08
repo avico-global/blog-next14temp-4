ec43bd14f8e82eca17d90713bc18401ccfe67acd
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-07-08 03:41:22 -0700

    NEXT CONFIG FILE

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "apisitem.ecommcube.com",
+        hostname: "api.sitebuilderz.com",
       },
     ],
   },
